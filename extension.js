44ca37b36f82e9e0068044479cd970f99bae612f
Mr Lima <felipebr4095@gmail.com>
2025-10-24 22:06:21 -0300

    Package

---
+++ extension.js
@@ -0,0 +1,12 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log("J2ME extension activated");
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
